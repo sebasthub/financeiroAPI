aabeb5d9112fc5b09ac828ec834b45f60320cf2d
sebasthub <playandroidhb@gmail.com>
2024-06-02 22:32:50 -0300

    fix: profiler

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import sentry_sdk
 
 from . import models
 from app import router
@@ -7,6 +9,31 @@ from .database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
+sentry_sdk.init(
+    dsn="",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
+
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(router.api_router)
+
+
+@app.get("/sentry-debug")
+async def trigger_error():
+    division_by_zero = 1 / 0
